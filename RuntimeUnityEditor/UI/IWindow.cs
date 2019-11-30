fde7e595589613730ed95669c142475b2fb7da6f
Brett Taylor <bretttaylor1998@live.co.uk>
2019-11-30 16:02:59 +0000

    Started the window interface

---
+++ RuntimeUnityEditor/UI/IWindow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace RuntimeUnityEditor.Core.UI
+{
+    internal interface IWindow
+    {
+        bool VisibleOnlyWhenEditorActive { get; set; }
+        void UpdateWindowSize(Rect screenSize);
+        void DisplayWindow();
+    }
+}
